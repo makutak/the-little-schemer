35ed053784e39da75b14f5a233aefb94f0db3e82
makutak <takkouno@gmail.com>
2018-05-18 00:48:05 +0900

    指定したアトムを除外したリストを作る

--- ch-04.scm
+++ ch-04.scm
@@ -174,3 +174,16 @@
 (test-equal 'macaroni (pick 4 '(lasagna spaghetti ravioli macaroni meatball)))
 (test-error (pick 0 '(a)))
 (test-end "pick-test")
+
+(define rempick
+  (lambda (n lat)
+    (cond
+     ((zero? (sub1 n)) (cdr lat))
+     (else
+      (cons
+       (car lat)
+       (rempick (sub1 n) (cdr lat)))))))
+
+(test-begin "rempick-test")
+(test-equal '(hotdogs with mustard) (rempick 3 '(hotdogs with hot mustard)))
+(test-end"rempick-test")
