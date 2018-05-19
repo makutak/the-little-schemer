08b143b47c889d21fb668bccb9e61ad140777dae
makutak <takkouno@gmail.com>
2018-05-20 02:44:11 +0900

    ネストしたリストから指定したアトムを除去したリストを返す

---
+++ ch-05.scm
@@ -0,0 +1,32 @@
+#!/usr/local/bin/guile
+!#
+
+(add-to-load-path "./lib/")
+(import (srfi srfi-64) (util))
+
+(set! test-log-to-file #f)
+
+(define rember*
+  (lambda (a l)
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) a)
+        (rember* a (cdr l)))
+       (else
+        (cons
+         (car l)
+         (rember* a (cdr l))))))
+     (else
+      (cons
+       (rember* a (car l))
+       (rember* a (cdr l)))))))
+
+(test-begin "rember*-test")
+(test-equal '((coffee) ((tea))
+              (and (hick)))
+            (rember* 'cup
+                     '((coffee) cup ((tea) cup)
+                       (and (hick)) cup)))
+(test-end "rember*-test")
