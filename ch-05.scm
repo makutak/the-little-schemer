4a0c546eb8a258c7b2c46c21bf675a8b22c0f397
makutak <takkouno@gmail.com>
2018-05-21 01:29:57 +0900

    atomの右に指定した値を挿入する

--- ch-05.scm
+++ ch-05.scm
@@ -40,22 +40,37 @@
 
 (define insertR*
   (lambda (new old l)
-    "insertR*"))
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons old
+              (cons new
+                    (insertR* new old (cdr l)))))
+       (else
+        (cons
+         (car l)
+         (insertR* new old (cdr l))))))
+     (else
+      (cons
+       (insertR* new old (car l))
+       (insertR* new old (cdr l)))))))
 
 (test-begin "insertR*-test")
 (test-equal '((how much (wood))
               could
-              ((a (wood)))
-              (((chuck)))
-              (if (a) ((wood)))
-              (could wood))
+              ((a (wood) chuck roast))
+              (((chuck roast)))
+              (if (a) ((wood chuck  roast)))
+              (could chuck roast wood))
   (insertR* 'roast
             'chuck
             '((how much (wood))
               could
-              ((a (wood) chunk))
+              ((a (wood) chuck))
               (((chuck)))
-              (if (a) ((wood chunk)))
+              (if (a) ((wood chuck)))
               (could chuck wood))))
 
 (test-end "insertR*-test")
