435c15d45ef03523cbf2bd8b9dd7863d68dd6cbc
makutak <takkouno@gmail.com>
2018-05-22 00:37:51 +0900

    関数実装

--- ch-05.scm
+++ ch-05.scm
@@ -138,7 +138,20 @@
 
 (define insertL*
   (lambda (new old l)
-    "insertL*"))
+    (cond
+     ((null? l) (quote ()))
+     ((atom? (car l))
+      (cond
+       ((eq? (car l) old)
+        (cons new
+              (cons old
+                    (insertL* new old (cdr l)))))
+       (else
+        (cons (car l) (insertL* new old (cdr l))))))
+     (else
+      (cons
+       (insertL* new old (car l))
+       (insertL* new old (cdr l)))))))
 
 (test-begin "insertL*-test")
 (test-equal '((how much (wood))
