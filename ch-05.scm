50f03a6aa18dd8adc7be36c378290819be993d81
makutak <takkouno@gmail.com>
2018-05-23 02:59:36 +0900

    リストが等しいか比較する関数

--- ch-05.scm
+++ ch-05.scm
@@ -233,7 +233,19 @@
 
 (define eqlist?
   (lambda (l1 l2)
-    #t))
+    (cond
+     ((and (null? l1) (null? l2)
+           #t))
+     ((or (null? l1) (null? l2)
+          #f))
+     ((and (atom? (car l1)) (atom? (car l2)))
+      (and (eqan? (car l1) (car l2))
+           (eqlist? (cdr l1) (cdr l2))))
+     ((or (atom? (car l1)) (atom? (car l2))
+          #f))
+     (else
+      (eqlist? (car l1) (car l2))
+      (eqlist? (cdr l1) (cdr l2))))))
 
 (test-begin "eqlist?-test")
 (test-equal #t
