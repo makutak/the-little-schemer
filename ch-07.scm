dc847f897da489130f35e08cd2738349c8271633
makutak <takkouno@gmail.com>
2018-05-26 04:59:04 +0900

    これは集合ですか?

--- ch-07.scm
+++ ch-07.scm
@@ -5,3 +5,18 @@
 (import (srfi srfi-64) (util))
 
 (set! test-log-to-file #f)
+
+(define set?
+  (lambda (lat)
+    #t))
+(test-begin "set?-test")
+
+(let ((lat '(apple peaches apple plum)))
+  (test-equal #f
+    (set? lat)))
+
+(let ((lat '(apples peaches pears plums)))
+  (test-equal #t
+    (set? lat)))
+
+(test-end "set?-test")
