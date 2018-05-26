1609d92aa26123cceeb96269a3023fb31db00d5e
makutak <takkouno@gmail.com>
2018-05-27 03:58:51 +0900

    andを使って書き直した

--- ch-07.scm
+++ ch-07.scm
@@ -80,10 +80,9 @@
   (lambda (set1 set2)
     (cond
      ((null? set1) #t)
-     ((member? (car set1) set2)
-      (subset? (cdr set1) set2))
      (else
-      #f))))
+      (and (member? (car set1) set2)
+           (subset? (cdr set1) set2))))))
 
 (test-begin "subset?-test")
 
