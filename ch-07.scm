ffa19e4af407a0cb47905a93f5100521c4d408a3
makutak <takkouno@gmail.com>
2018-05-27 17:12:04 +0900

    関数実装

--- ch-07.scm
+++ ch-07.scm
@@ -119,7 +119,11 @@
 
 (define intersect?
   (lambda (set1 set2)
-    #t))
+    (cond
+     ((null? set1) #f)
+     (else
+      (or (member? (car set1) set2)
+          (intersect? (cdr set1) set2))))))
 
 (test-begin "intersect?-test")
 
