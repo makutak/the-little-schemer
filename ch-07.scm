92a13d4cf71ebea26d0356c82c8b8b12c8e92244
makutak <takkouno@gmail.com>
2018-05-27 17:27:25 +0900

    和集合を作る関数

--- ch-07.scm
+++ ch-07.scm
@@ -166,7 +166,13 @@
 ;;union: 和集合
 (define union
   (lambda (set1 set2)
-    set2))
+    (cond
+     ((null? set1) set2)
+     ((member? (car set1) set2)
+      (union (cdr set1) set2))
+     (else
+      (cons (car set1)
+            (union (cdr set1) set2))))))
 
 (test-begin "union-test")
 
