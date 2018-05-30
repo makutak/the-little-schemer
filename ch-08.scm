945d0e9c8c202ee1807f0d4813970bd832d8a0c0
makutak <takkouno@gmail.com>
2018-05-31 02:45:56 +0900

    関数に引数として関数を渡し、要素の比較を行うようにしたい

--- ch-08.scm
+++ ch-08.scm
@@ -5,3 +5,20 @@
 (import (srfi srfi-64) (util))
 
 (set! test-log-to-file #f)
+
+(define rember-f
+  (lambda (test? a l)
+    "rember-f"))
+
+(test-begin "rember-f-test")
+
+(test-equal '(6 2 3)
+  (rember-f = 5 '(6 2 5 3)))
+
+(test-equal '(beans are good)
+  (rember-f eq? 'jelly '(jelly beans are good)))
+
+(test-equal '(lemonade (pop corn) and (cake))
+  (rember-f equal?? '(pop corn) '(lemonade (pop corn) and (cake))))
+
+(test-end "rember-f-test")
