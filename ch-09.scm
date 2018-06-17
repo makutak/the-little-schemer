b60ce170c6e48b246dddc839e1ad3971ae23553f
makutak <takkouno@gmail.com>
2018-06-17 23:01:47 +0900

    latから要素を見つける関数を定義したい

--- ch-09.scm
+++ ch-09.scm
@@ -5,3 +5,17 @@
 (import (srfi srfi-64) (util))
 
 (set! test-log-to-file #f)
+
+(define looking
+  (lambda (a lat)
+    #t))
+
+(test-begin "looking-test")
+(test-equal #t
+  (looking 'caviar
+           '(6 2 4 caviar 5 7 3)))
+
+(test-equal #f
+  (looking 'caviar
+           '(6 2 grits caviar 5 7 3)))
+(test-end "looking-test")
