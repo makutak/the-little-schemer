8120f137cee9d84d51664a2d3d6d60a7acc24c8c
makutak <takkouno@gmail.com>
2018-07-08 03:36:38 +0900

    これが正しい関数ですか。はい。

--- ch-09.scm
+++ ch-09.scm
@@ -822,15 +822,15 @@
 
 (test-end "mk-length-test-again")
 
-((lambda (mk-length)
-   (mk-length mk-length))
- (lambda (mk-length)
-   ((lambda (length)
-       (lambda (l)
-         (cond
-          ((null? l) 0)
-          (else
-           (add1
-            (length (cdr l)))))))
-    (lambda (x)
-      ((mk-length mk-length) x)))))
+((lambda (le)
+   ((lambda (mk-length)
+      (mk-length mk-length))
+    (lambda (mk-length)
+      (le (lambda (x)
+            ((mk-length mk-length) x))))))
+ (lambda (length)
+   (lambda (l)
+     (cond
+      ((null? l) 0)
+      (else
+       (add1 (length (cdr l))))))))
