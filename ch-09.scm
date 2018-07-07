0d9d91e705892bdffd175a64b7b98e5077c21b7c
makutak <takkouno@gmail.com>
2018-07-08 03:26:25 +0900

    考え直そう

--- ch-09.scm
+++ ch-09.scm
@@ -825,12 +825,9 @@
 ((lambda (mk-length)
    (mk-length mk-length))
  (lambda (mk-length)
-   ((lambda (length)
-      (lambda (l)
-        (cond
-         ((null? l)
-          0)
-         (else
-          (add1
-           (length (cdr l)))))))
-    (mk-length mk-length))))
+   (lambda (l)
+     (cond
+      ((null? l) 0)
+      (else
+       (add1
+        (length (cdr l))))))))
