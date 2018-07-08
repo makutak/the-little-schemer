528fbcb93c329730a0e090bc8bbf2899b5c3f8cc
makutak <takkouno@gmail.com>
2018-07-09 01:33:23 +0900

    値のリストに対してkeyが与えられたら、それに対応する値を返す。

--- ch-10.scm
+++ ch-10.scm
@@ -5,3 +5,34 @@
 (import (srfi srfi-64) (util))
 
 (set! test-log-to-file #f)
+
+;;entry: 連想配列のようなもの
+
+(define lookup-in-entry-help
+  (lambda (name names values entry-f)
+    (cond
+     ((null? names) (entry-f name))
+     ((eq? (car names) name)
+      (car values))
+     (else
+      (lookup-in-entry-help name
+                            (cdr names)
+                            (cdr values)
+                            entry-f)))))
+
+(define lookup-in-entry
+  (lambda (name entry entry-f)
+    (lookup-in-entry-help name
+                          (first entry)
+                          (second entry)
+                          entry-f)))
+
+(test-begin "lookup-in-entry-test")
+
+(test-equal 'tastes
+  (lookup-in-entry 'entree
+                   '((appetier entree beverage)
+                     (food tastes good))
+                   (lambda (x) '())))
+
+(test-end "lookup-in-entry-test")
